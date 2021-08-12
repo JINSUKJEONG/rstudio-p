7da2f32f6ea3708702eeafb0ccc150a6d189a550
jjs project <diplojoung@gmail.com>
2021-08-12 22:00:44 +0900

    pnorm

---
+++ test2.R
@@ -0,0 +1,14 @@
+print("hello")
+
+# about probability
+# https://statkclee.github.io/r-algorithm/r-probability-exercise.html
+# pnorm function. calculation of probability
+
+pnorm(1.9, mean=1.7, sd = 0.1)
+1-pnorm(1.6, mean = 1.7, sd=0.1)
+
+
+# 난수 함수 rnorm
+# 확률밀도함수 dnorm
+# 누적분포함수 pnorm
+# 분위수함수 qnorm
